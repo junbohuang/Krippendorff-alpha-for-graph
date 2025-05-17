eee51bd7f29b70a6f1b3a69ad04385908c5bed1f
Junbo Huang <junbo.huang@uni-hamburg.de>
2025-05-18 01:05:19 +0200

    :sparkles: added revision

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='krippendorrf-graph',
-    version='0.1.0',
+    version='0.2.0',
     description='A Python package for computing krippendorrfs alpha for graph (modified from https://github.com/grrrr/krippendorff-alpha/blob/master/krippendorff_alpha.py)',
     url='https://github.com/junbohuang/Krippendorrf-alpha-for-graph',
     author='Junbo Huang',
