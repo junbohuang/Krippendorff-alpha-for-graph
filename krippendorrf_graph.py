2fb0eedd2c8636d4bc22ea07505930dc528e2cd1
Junbo Huang <junbo.huang@uni-hamburg.de>
2025-05-18 01:03:01 +0200

    :sparkles: added revision

--- krippendorrf_graph.py
+++ krippendorrf_graph.py
@@ -1,75 +1,87 @@
-import json
-from pathlib import Path
 
 import numpy as np
 import networkx as nx
 from tqdm import tqdm
+from typing import Optional, Union, Callable
 
 
-def nominal_metric(a, b):
-    return a != b
-
-
-def graph_overlap_metric(a, b):
-    return len(a & b) == 0
-
-
-def graph_edit_distance(a, b):
-    return compute_normalized_graph_edit_distance(a, b)
-
-
-def instantiate_networkx_graph(triples: list[tuple]):
-    G = nx.DiGraph()
+def instantiate_networkx_graph(triples: list[tuple], graph_type=nx.MultiDiGraph):
+    G = graph_type()
     for (subj, rel, obj) in triples:
         G.add_edge(subj, obj, relation=rel)
     return G
 
 
-def compute_normalized_graph_edit_distance(triples_1: list[tuple], triples_2: list[tuple]):
+def nominal_metric(a, b, graph_type=None):
+    return a != b
+
+
+def node_overlap_metric(a, b, graph_type=None):
+    return len(a & b) == 0
+
+
+def graph_overlap_metric(triples_1: list[tuple], triples_2: list[tuple], graph_type=None):
+    """
+    If two graphs overlaps, distance = 0, else 1.
+    """
+    return len(list(set(triples_1) & set(triples_2))) == 0
+
+
+def graph_edit_distance(triples_1: list[tuple], triples_2: list[tuple],
+                        graph_type: Optional[
+                            Union[nx.Graph, nx.DiGraph, nx.MultiGraph, nx.MultiDiGraph]] = nx.MultiDiGraph):
+    """
+    Normalized graph edit distance from networkx.
+    """
     g0 = nx.empty_graph()
-    g1 = instantiate_networkx_graph(triples_1)
-    g2 = instantiate_networkx_graph(triples_2)
-    normalized_ged = nx.graph_edit_distance(g1, g2)/(nx.graph_edit_distance(g1, g0)+nx.graph_edit_distance(g2, g0))
+    g1 = instantiate_networkx_graph(triples_1, graph_type)
+    g2 = instantiate_networkx_graph(triples_2, graph_type)
+    normalized_ged = nx.graph_edit_distance(g1, g2) / (nx.graph_edit_distance(g1, g0) + nx.graph_edit_distance(g2, g0))
     return normalized_ged
 
 
-def interval_metric(a, b):
-    return ( a -b )**2
+def compute_distance_matrix(df, feature_column: str,
+                            graph_distance_metric: Callable,
+                            empty_graph_indicator: str = "*",
+                            save_path: Optional[str] = "./distance_matrix.npy",
+                            graph_type: Optional[
+                                Union[nx.Graph, nx.DiGraph, nx.MultiGraph, nx.MultiDiGraph]] = nx.MultiDiGraph):
+    """
+    Compute distance matrix with custom graph distance metric from a pandas data frame object.
+    A graph must be represented as a list of tuple, such as [("subject_1", "predicate_1", "object_1"), ("subject_2", "predicate_2", "object_2")]
+
+    :param df: a pandas data frame object containing a feature_column storing all graph annotations for all annotators.
+    :param feature_column: name of the column storing all graph annotations for all annotators.
+    :param graph_distance_metric: a callable function to compute distance between any two graphs.
+    :param empty_graph_indicator: a string indicating an empty graph
+    :param save_path: local path to store the computed distance matrix
+    :param graph_type: a networkx graph type. Bear in mind the interaction between graph type and graph distance metric.
+    :return: distance matrix as numpy array.
+    """
+    distance_matrix = np.zeros(shape=(len(df[feature_column]), len(df[feature_column])))
+    computed_indices = set()
+    for i, g1 in tqdm(enumerate(df[feature_column])):
+        for j, g2 in enumerate(df[feature_column]):
+            if {i, j} not in computed_indices:
+                if g1 == empty_graph_indicator or g2 == empty_graph_indicator:
+                    # ignore missing graph annotation by assign 0 distance to other graphs for faster computation by observed and expected disagreement.
+                    distance_matrix[i][j] = 0
+                else:
+                    distance_matrix[i][j] = graph_distance_metric(g1, g2, graph_type=graph_type)
+                computed_indices.update({i, j})
+            else:
+                distance_matrix[i][j] = distance_matrix[j][i]
+    with open(save_path, 'wb') as f:
+        np.save(f, distance_matrix)
+    return distance_matrix
 
 
-def ratio_metric(a, b):
-    return (( a -b ) /( a +b) )**2
-
-
-def compute_alpha(data, metric=interval_metric, force_vecmath=False, convert_items=float, missing_items=None, distance_matrix_path=None):
-    '''
-    Calculate Krippendorff's alpha (inter-rater reliability):
-
-    data is in the format
-    [
-        {unit1:value, unit2:value, ...},  # coder 1
-        {unit1:value, unit3:value, ...},   # coder 2
-        ...                            # more coders
-    ]
-    or
-    it is a sequence of (masked) sequences (list, numpy.array, numpy.ma.array, e.g.) with rows corresponding to coders and columns to items
-
-    metric: function calculating the pairwise distance
-    force_vecmath: force vector math for custom metrics (numpy required)
-    convert_items: function for the type conversion of items (default: float)
-    missing_items: indicator for missing items (default: None)
-    '''
-
-    # number of coders
-    m = len(data)
-
+def construct_units(data: list[dict], missing_items=None):
     # set of constants identifying missing values
     if missing_items is None:
         maskitems = []
     else:
         maskitems = list(missing_items)
-    if np is not None:
-        maskitems.append(np.ma.masked_singleton)
 
     # convert input data to a dict of items
     units = {}
@@ -88,138 +100,54 @@ def compute_alpha(data, metric=interval_metric, force_vecmath=False, convert_ite
                 except KeyError:
                     its = []
                     units[it] = its
-                its.append(convert_items(g))
-
+                its.append(set(g))
+    # A unit is a set of annotation per item
     units = dict((it, d) for it, d in units.items() if len(d) > 1)  # units with pairable values
+    return units
 
+
+def compute_alpha(data: list[dict], distance_matrix: np.array, missing_items=None):
+    '''
+    Compute Krippendorrf's alpha for graph.
+    Modified from https://github.com/grrrr/krippendorff-alpha/tree/master
+        1. strictly enforce data to be of type list of dict
+        2. requires now a distance matrix for efficient computation
+
+    data is in the format
+    [
+        {unit1:value, unit2:value, ...},  # coder 1
+        {unit1:value, unit3:value, ...},   # coder 2
+        ...                            # more coders
+    ]
+    distance_matrix: a numpy array of size number_items*number_items
+    missing_items: indicator for missing items (default: None)
+    '''
+    number_annotator = len(data)
+    item_per_annotator = len(data[0])
+    units = construct_units(data, missing_items)
     n = sum(len(pv) for pv in units.values())  # number of pairable values
     if n == 0:
         raise ValueError("No items to compare.")
 
-    np_metric = (np is not None) and ((metric in (interval_metric, nominal_metric, ratio_metric)) or force_vecmath)
-
-    Do = 0. # disagreement observed
-
-    if metric == graph_edit_distance:
-        if distance_matrix_path is not None and Path(distance_matrix_path).exists():
-            with open(distance_matrix_path, "r") as f:
-                graph_distance_dict = json.load(f)
-        else:
-            graph_distance_dict = {}
-            for grades in tqdm(units.values()):
-                for gj in grades:
-                    for gi in grades:
-                        key = str(gi)+str(gj)
-                        if key not in graph_distance_dict:
-                            if str(gi) == str(gj):
-                                graph_distance_dict[key] = 0
-                            else:
-                                graph_distance_dict[key] = metric(gi, gj)
-
-    for grades in units.values():
-        if np_metric:
-            gr = np.asarray(grades)
-            Du = sum(np.sum(metric(gr, gri)) for gri in gr)
-        else:
-            if metric == graph_edit_distance:
-                Du = sum(graph_distance_dict[str(gi)+str(gj)] for gi in grades for gj in grades)
-            else:
-                Du = sum(metric(gi, gj) for gi in grades for gj in grades)
-
-        Do += Du /float(len(grades ) -1)
+    # Compute within-unit disagreement
+    Do = 0
+    for item, grades in units.items():
+        indices_of_item_all_annotator = [item + (item_per_annotator * i) for i in range(number_annotator)]
+        indices = [(i, j) for i in indices_of_item_all_annotator for j in indices_of_item_all_annotator if i != j]
+        Du = sum([distance_matrix[i, j] for (i, j) in indices])
+        Do += Du / float(len(grades) - 1)
 
     Do /= float(n)
-    if Do == 0:
-        return 1.
 
-    De = 0. # disagreement expected by chance
-    for g1 in units.values():
-        if np_metric:
-            d1 = np.asarray(g1)
-            for g2 in units.values():
-                De += sum(np.sum(metric(d1, gj)) for gj in g2)
-        else:
-            for g2 in units.values():
-                if metric == graph_edit_distance:
-                    for gi in g1:
-                        for gj in g2:
-                            if str(gi)+str(gj) not in graph_distance_dict:
-                                graph_distance_dict[str(gi) + str(gj)] = metric(gi, gj)
-                    De += sum(graph_distance_dict[str(gi)+str(gj)] for gi in g1 for gj in g2)
-                else:
-                    De += sum(metric(gi, gj) for gi in g1 for gj in g2)
-    De /= float(n * (n-1))
-    if distance_matrix_path is None:
-        with open("./graph_distance_matrix.json", "w") as f:
-            json.dump(graph_distance_dict, f)
-    else:
-        with open(distance_matrix_path, "w") as f:
-            json.dump(graph_distance_dict, f)
-    return 1.-Do/De if (Do and De) else 1.
+    # Compute within- and between-unit disagreement (expectation of total disagreement by chance)
+    De = 0
+    for item_1, g1 in units.items():
+        for item_2, g2 in units.items():
+            indices_of_item_all_annotator_1 = [item_1 + (item_per_annotator * i) for i in range(number_annotator)]
+            indices_of_item_all_annotator_2 = [item_2 + (item_per_annotator * i) for i in range(number_annotator)]
+            indices = [(i, j) for i in indices_of_item_all_annotator_1 for j in indices_of_item_all_annotator_2]
+            De += sum(distance_matrix[gi, gj] for (gi, gj) in indices)
+    De /= float(n * (n - 1))
+    alpha = 1 - Do / De if (Do and De) else 1
+    return alpha
 
-
-if __name__ == '__main__':
-    data = [
-        [
-            {('Monetary Policy', 'Increases', 'Inflation')},
-            {('Wages', 'Increases', 'Inflation')},
-            {('Energy Prices', 'Increases', 'Inflation'), ('Medical Costs', 'Increases', 'Inflation'), ('Tax Increases', 'Increases', 'Inflation')},
-            {('Demand (residual)', 'Increases', 'Wages'), ('Wages', 'Increases', 'Inflation')},
-            {('Government Debt', 'Increases', 'Inflation'), ('Monetary Policy', 'Decreases', 'Inflation')},
-            {('Tax Increases', 'Increases', 'Inflation'), ('Wages', 'Increases', 'Inflation Expectations')},
-            {('Government Debt', 'Increases', 'Inflation'), ('Monetary Policy', 'Decreases', 'Government Debt')},
-            {('Energy Prices', 'Increases', 'Inflation'), ('Food Prices', 'Increases', 'Inflation'), ('Food Prices', 'Increases', 'Inflation Expectations'), ('Medical Costs', 'Increases', 'Inflation'), ('Supply (residual)', 'Increases', 'Inflation')},
-            {('Pent-up Demand', 'Increases', 'Inflation')}, {('Wages', 'Increases', 'Inflation')}],
-        [
-            {('Monetary Policy', 'Increases', 'Inflation')},
-            {('Wages', 'Increases', 'Inflation')},
-            {('Energy Prices', 'Increases', 'Inflation'),
-               ('Food Prices', 'Increases', 'Inflation'),
-               ('Inflation', 'Increases', 'Energy Prices'),
-               ('Inflation Expectations', 'Increases', 'Inflation'),
-               ('Medical Costs', 'Increases', 'Inflation'),
-               ('Supply (residual)', 'Decreases', 'Inflation'),
-               ('Tax Increases', 'Increases', 'Inflation'),
-               ('Transportation Costs', 'Decreases', 'Inflation')},
-            {('Demand (residual)', 'Increases', 'Inflation'), ('Monetary Policy', 'Decreases', 'Inflation'), ('Wages', 'Increases', 'Inflation')},
-            {('Government Debt', 'Increases', 'Inflation'), ('Monetary Policy', 'Decreases', 'Inflation'), ('Pent-up Demand', 'Increases', 'Inflation'), ('Supply (residual)', 'Increases', 'Inflation')},
-            {('Tax Increases', 'Increases', 'Inflation'), ('Wages', 'Increases', 'Inflation')},
-            {('Government Spending', 'Increases', 'Inflation')},
-            {('Energy Prices', 'Increases', 'Inflation'),
-               ('Food Prices', 'Increases', 'Inflation'),
-               ('Medical Costs', 'Increases', 'Inflation'),
-               ('Supply (residual)', 'Increases', 'Inflation'),
-               ('Transportation Costs', 'Increases', 'Inflation')},
-            {('Inflation Expectations', 'Increases', 'Inflation'), ('Pent-up Demand', 'Increases', 'Inflation')},
-            {('Wages', 'Increases', 'Inflation')}],
-        [
-            {('Monetary Policy', 'Increases', 'Inflation')},
-            {('Inflation', 'Decreases', 'Monetary Policy'), ('Wages', 'Increases', 'Inflation')},
-            {('Medical Costs', 'Increases', 'Inflation'), ('Tax Increases', 'Increases', 'Medical Costs'), ('War', 'Increases', 'Energy Prices')},
-            {('Demand (residual)', 'Increases', 'Inflation'), ('Inflation', 'Increases', 'Wages'), ('Monetary Policy', 'Decreases', 'Demand (residual)'), ('Wages', 'Increases', 'Inflation')},
-            {('Demand (residual)', 'Increases', 'Inflation'), ('Government Spending', 'Increases', 'Inflation')},
-            {('Tax Increases', 'Increases', 'Inflation'), ('Wages', 'Increases', 'Inflation')},
-            {('Government Spending', 'Decreases', 'Demand (residual)'), ('Government Spending', 'Increases', 'Inflation'), ('Inflation Expectations', 'Increases', 'Monetary Policy'), ('Monetary Policy', 'Decreases', 'Government Spending')},
-            {('Climate', 'Increases', 'Supply (residual)'),
-               ('Energy Prices', 'Increases', 'Inflation'),
-               ('Food Prices', 'Increases', 'Inflation'),
-               ('Medical Costs', 'Increases', 'Inflation'),
-               ('Supply (residual)', 'Increases', 'Food Prices')},
-            {('Pent-up Demand', 'Increases', 'Inflation')},
-            {('Inflation', 'Increases', 'Wages'), ('Wages', 'Increases', 'Inflation')}],
-        [
-            {('Monetary Policy', 'Increases', 'Inflation')},
-            {('Inflation', 'Decreases', 'Inflation')},
-            {('Energy Prices', 'Increases', 'Inflation'), ('Monetary Policy', 'Increases', 'Inflation'), ('Price-Gouging', 'Increases', 'Inflation'), ('Tax Increases', 'Increases', 'Inflation')},
-            {('Demand (residual)', 'Increases', 'Inflation'), ('Inflation', 'Decreases', 'Demand (residual)'), ('Inflation', 'Increases', 'Inflation')},
-            {('Monetary Policy', 'Decreases', 'Inflation'), ('Monetary Policy', 'Increases', 'Inflation Expectations')},
-            {('Tax Increases', 'Increases', 'Inflation'), ('Wages', 'Increases', 'Inflation')},
-            {('Mismanagement', 'Increases', 'Inflation')},
-            {('Energy Prices', 'Increases', 'Inflation'), ('Food Prices', 'Increases', 'Inflation')},
-            {('Pent-up Demand', 'Increases', 'Inflation')},
-            {('Wages', 'Increases', 'Inflation')}]
-    ]
-
-    missing = "*" # indicator for missing values
-
-    print("Gragh edit distance: %.3f" % compute_alpha(data, graph_edit_distance, missing_items=missing, convert_items=set, distance_matrix_path="./distance_matrix.json"))
--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
-    name='graph-krippendorrf',
+    name='krippendorrf-graph',
     version='0.1.0',
     description='A Python package for computing krippendorrfs alpha for graph (modified from https://github.com/grrrr/krippendorff-alpha/blob/master/krippendorff_alpha.py)',
-    url='https://github.com/grrrr/krippendorff-alpha/blob/master/krippendorff_alpha.py',
+    url='https://github.com/junbohuang/Krippendorrf-alpha-for-graph',
     author='Junbo Huang',
     author_email='junbo.huang@uni-hamburg.de',
     license='Apache 2 License',
@@ -12,5 +17,7 @@ setup(
                       'numpy',
                       "tqdm"
                       ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     py_modules=["krippendorrf_graph"]
 )
