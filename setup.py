a37adb4f3d757d74adbbf009ee80592854d95d84
Junbo Huang <junbo.huang@uni-hamburg.de>
2025-10-24 18:40:22 +0200

    :sparkle: added lrec version

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup
-
+import os
 # read the contents of your README file
 from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
-
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+#print(os.listdir())
+#with open('requirements.txt') as f:
+requirements = ['pandas', 'scikit-learn', 'requests', 'networkx', 'tqdm', 'numpy']
 
 setup(
     name='krippendorff-graph',
-    version='0.1.3',
+    version='0.1.4',
     description='A Python package for computing krippendorffs alpha for graph (modified from https://github.com/grrrr/krippendorff-alpha/blob/master/krippendorff_alpha.py)',
-    url='anonymous url',
+    url='https://anonymous.4open.science/r/BE2B/',
     author='anonymous author',
-    author_email='anonymous email',
+    author_email='anonymous@email.com',
     license='Apache 2 License',
     install_requires=requirements,
     long_description=long_description,
